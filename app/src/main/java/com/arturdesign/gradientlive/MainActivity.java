b0067f678500439048ececba2c2778faff8fbbaf
artur-design <nukleus193@gmail.com>
2026-02-06 08:26:42 +0500

    Update MainActivity.java

--- app/src/main/java/com/arturdesign/gradientlive/MainActivity.java
+++ app/src/main/java/com/arturdesign/gradientlive/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.gradient;
+package com.arturdesign.gradientlive;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ComponentName;
 import android.content.Intent;
